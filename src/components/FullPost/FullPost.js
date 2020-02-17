7a1d3c6f979598af1892f74431a7c309c49d1d72
Simon Atkins <simon@flickapp.com>
2020-02-17 16:38:09 +0000

    Fix bug with infinite API calls happening due to axios being re-run every time state is updated

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -4,22 +4,27 @@ import './FullPost.css';
 
 class FullPost extends Component {
     state = {
-        loadedPost: ''
+        loadedPost: null
     };
 
     componentDidUpdate() {
         if (this.props.id) {
-            axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(
-                response => {
-                    this.setState({loadedPost: response.data})
-                }
-            )
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(
+                    response => {
+                        this.setState({loadedPost: response.data})
+                    }
+                )
+            }
         }
     }
 
     render() {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+        if (this.state.loadedPost) {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.loadedPost.title}</h1>
