88bce90b7d6d691e5d7e3ec5fde74f8e261305d2
Simon Atkins <simon@flickapp.com>
2020-02-17 17:20:01 +0000

    Add functionality to delete posts

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -19,6 +19,14 @@ class FullPost extends Component {
         }
     }
 
+    handleDelete = (id) => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(
+            response => {
+                console.log('DELETE response', response)
+            }
+        )
+    };
+
     render() {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.props.id) {
@@ -30,7 +38,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.handleDelete}>Delete</button>
                     </div>
                 </div>
             )
