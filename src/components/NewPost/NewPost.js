ab9faf43725abf3b2358eb6d8c88f4ef4279c219
Simon Atkins <simon@flickapp.com>
2020-02-17 17:00:11 +0000

    Add functionality to create new Post

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './NewPost.css';
 
 class NewPost extends Component {
@@ -7,7 +7,19 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
-    }
+    };
+
+    handleAddPost = () => {
+        const data = {
+            title: this.state.title,
+            content: this.state.content,
+            author: this.state.author
+        };
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+            .then(response => {
+                console.log(response)
+            })
+    };
 
     render () {
         return (
@@ -22,7 +34,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.handleAddPost}>Add Post</button>
             </div>
         );
     }
