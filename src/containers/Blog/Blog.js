8e07499dc8cb7151b40e4d20bbcbd9909d115ae4
Simon Atkins <simonatk@gmail.com>
2020-05-05 18:46:50 +0100

    Create HOC AsynComponent to allow lazy loading of NewPost component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from "../../hoc/asyncComponent";
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost')
+});
 
 class Blog extends Component {
     render () {
@@ -28,9 +32,10 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/new-post" component={AsyncNewPost} />
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>404 Not Found :(</h1>} />
+                    {/*<Redirect from="/" to="/posts" />*/}
                 </Switch>
             </div>
         );
